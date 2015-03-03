f1854b078f0d92d53b3725c82dc8399f9f8e1697
CHE-CHE <hjamilet64@gmail.com>
2015-03-03 11:22:28 +0100

    actualizar los datos

--- Proyecto/Prototipo del proyecto/datos escolares/borrar.php
+++
@@ -1,8 +0,0 @@
-<?php
-   include("conexion.php");
-   $link=Conectarse();
-   $id=$_GET['id'];
-   mysql_query("delete from DatosEscolares where matricula = $id",$link);
-   
-   header("Location: ejeborrar.php");
-?>
--- Proyecto/Prototipo del proyecto/materiascursadas/borrar.php
+++
@@ -1,8 +0,0 @@
-<?php
-   include("conexion.php");
-   $link=Conectarse();
-   $id=$_GET['id'];
-   mysql_query("delete from materiascursadas where matricula = $id",$link);
-   
-   header("Location: ejeborrar.php");
-?>
